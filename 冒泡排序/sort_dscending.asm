16971372ee77fe46acbef4c3aba762a5caf5ffbe
deepYY <596903325@qq.com>
2017-10-22 16:23:42 +0800

    Second experiment

---
+++ 冒泡排序/sort_dscending.asm
@@ -0,0 +1,59 @@
+title sort
+ include irvine32.inc
+ .data
+  tab db 12,9,4,5,7,3,1,8
+  count EQU ($-tab)
+  dab db count dup(?)
+ .code
+ main proc
+     ;输出原来数组
+      mov cx,count
+      mov si,0
+    put1:
+      movsx eax,tab[si]
+      call writeint
+      mov al,32
+      call writechar
+      inc si
+      loop put1
+      mov al,10
+      call writechar
+     ;数组赋值
+     mov cx,count
+     mov si,0
+    s:
+      mov bl,tab[si]
+      mov dab[si],bl
+      inc si
+      loop s
+      ;冒泡排序
+     mov cx,count
+     dec cx  
+    lop1:  
+         mov di,cx 
+         mov si,0   
+    lop2:    
+         mov al,dab[si]
+         cmp al,dab[si+1]
+         jg go_on 
+         xchg al,dab[si+1]
+         mov dab[si],al   
+    go_on:  
+         add si,1 
+         loop lop2   
+      
+      mov cx,di
+      loop lop1
+   ;输出到CRT
+      mov cx,count
+      mov si,0
+    put2:
+      movsx eax,dab[si]
+      call writeint
+      mov al,32
+      call writechar
+      inc si
+      loop put2
+   exit
+ main endp
+ end main
